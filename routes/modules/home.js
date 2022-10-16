02f58d1edac2fb2c6afee05668f9e896158063fa
Victor Cheng <weitungstyle@gmail.com>
2022-10-16 23:41:47 +0800

    refactor

--- routes/modules/home.js
+++ routes/modules/home.js
@@ -4,6 +4,7 @@ const router = express.Router()
 const Restaurant = require('../../models/restaurant')
 
 router.get('/', (req, res) => {
+  console.log(req)
   Restaurant.find()
     .lean()
     .sort({ _id: 'asc' })
